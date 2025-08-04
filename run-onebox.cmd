8d26713cf784488b94be99a7e51ff0b8bd4bd290
BohdanVovkotrub <bvovkotrub@gmail.com>
2025-08-04 20:42:38 +0300

    update run-onebox.cmd

--- run-onebox.cmd
+++ run-onebox.cmd
@@ -10,35 +10,20 @@ for /f "usebackq tokens=1,2 delims==" %%a in (.env) do (
 set "main_dir=%cd%"
 cd %main_dir%
 
+echo Loading Redis-Master ...
+call "run-redis-master.cmd"
 
-if not exist data (
-  mkdir data 
-)
-if not exist data\master  (
-  mkdir data\master 
-)
-if not exist data\slave (
-  mkdir data\slave
-)
+echo Loading Redis-Slave ...
+call "run-redis-slave.cmd"
 
-REM Generate docker-compose-redis.yml
-set "TEMPLATE_COMPOSE=compose.redis.template.yml"
-set "OUTPUT_COMPOSE=docker-compose-redis.yml"
+echo Loading Sentinel-1 ...
+call "run-redis-sentinel1.cmd"
 
-REM Copy template to final file
-copy /Y "%TEMPLATE_COMPOSE%" "%OUTPUT_COMPOSE%" >nul
+echo Loading Sentinel-2 ...
+call "run-redis-sentinel2.cmd"
 
-REM Add networks section to end of file
-echo. >> %OUTPUT_COMPOSE%
-echo networks: >> %OUTPUT_COMPOSE%
-echo   %DOCKER_NETWORK_NAME%: >> %OUTPUT_COMPOSE%
-echo     external: true >> %OUTPUT_COMPOSE%
-
-echo Creating network %DOCKER_NETWORK_NAME% if it is not exists
-docker network inspect %DOCKER_NETWORK_NAME% >nul 2>&1 || docker network create %DOCKER_NETWORK_NAME%
-
-
-docker-compose -f docker-compose-redis.yml up -d 
+echo Loading Sentinel-3 ...
+call "run-redis-sentinel2.cmd"
 
 endlocal
 exit /b
